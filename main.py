f488422ee9837160ac360571b45222ff92b0661a
Karin van Homelen <karinvanhomelen@live.nl>
2022-06-06 19:39:07 +0200

    making code more clear for future reference

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import config
 from twilio.rest import Client
 
-account_sid = config.ACCOUNT_SID
-auth_token = config.AUTH_TOKEN
-twilio = config.TWILIO
-my_no = config.MY_NO
+account_sid = config.TWILIO_ACCOUNT_SID
+auth_token = config.TWILIO_AUTH_TOKEN
+twilio = config.TWILIO_NO
+my_no = config.MY_PHONE_NO
 client = Client(account_sid, auth_token)
 
-message = client.messages.create(from_=twilio, body="Hellooooow", to=my_no)
+message = client.messages.create(
+	from_=twilio,
+	body="Hellooooow",
+	to=my_no)
+
+
+print(message.sid)
